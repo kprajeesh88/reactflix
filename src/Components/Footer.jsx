afa2eae8865f70eefc663a025f370533cea0626e
Rajeesh kp <rajeesh.kp@xindus.net>
2023-04-03 11:48:01 +0530

    Added files

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "./Wrapper"
+export default function Footer(){
+    return(
+        <footer>
+             <Wrapper>
+                <p style={{textAlign:"center"}}>Movie website built with ReactJS</p>
+            </Wrapper>
+        </footer>
+    )
+}
